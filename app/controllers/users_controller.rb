a637946293408471715cc71c48187f4eecb0bfda
Matthew Stead <mattstead6@gmail.com>
2022-05-10 14:39:37 -0400

    omre

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,13 @@ class UsersController < ApplicationController
         end
       end
 
-
+ 
 
       #SIGN UP
 def create
     user = User.create!(user_params)
     if user.valid?
+        session[:user_id] = user.id
         render json: user, status: :created
     else
         render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
